d7b7f94bd67ebe2151714ae7a23dd09e2efd99d3
Krzymnjoe <krzymnjoe@gmail.com>
2024-11-16 17:42:25 -0500

    update drag
    dra

---
+++ Assets/Scripts/UI/Draggable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IDragHandler
+{
+    public Canvas canvas;
+
+    private RectTransform rectTransform;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    
+}
