3bd5039646eb5bc22a4f4a42b8aa21e6bd80c646
BrianStrigel <greatswordstudios@gmail.com>
2024-11-16 09:32:30 -0500

    scene maganger commit

---
+++ Assets/Scripts/SceneMgr.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMgr : MonoBehaviour
+{
+    float StartingWaterML = 500
+    float StartingElecPwerMW = 0
+    float StartingBuildingMaterialsKT = 50;
+    float StartingOxygenKT = 50;
+    float StartingBiomassKT = 1;
+    float TotalDrainRateWater = 0;
+    float TotalDrainRateElec = 0;
+    float TotalDrainRateOxygen = 0;
+    float TotalDrainRateBio = 0;
+    float TotalDrainRate = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
