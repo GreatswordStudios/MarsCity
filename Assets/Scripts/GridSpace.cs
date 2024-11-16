a449c937953c4ca7ac1a24d5a136cf1e7cee85f8
Jack Drolet <79327056+J-Drolet@users.noreply.github.com>
2024-11-16 11:55:04 -0500

    Can hover over grid now

--- Assets/Scripts/GridSpace.cs
+++ Assets/Scripts/GridSpace.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class GridSpace : MonoBehaviour
-{
+{   
+    Collider collider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        collider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var mousePos = Input.mousePosition;
+        var mouseRay = Camera.main.ScreenPointToRay(mousePos);
+        if (collider.bounds.IntersectRay(mouseRay)){
+            GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+        else {
+            GetComponent<MeshRenderer>().material.color = Color.white;
+        }
     }
 }
