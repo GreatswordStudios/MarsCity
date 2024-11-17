9761adcd4863736f49ebce6877ba3063094c6c64
Jack Drolet <79327056+J-Drolet@users.noreply.github.com>
2024-11-17 13:57:11 -0500

    Fix inventory bugs

--- Assets/Scripts/Inventorymanager.cs
+++ Assets/Scripts/Inventorymanager.cs
@@ -4,29 +4,56 @@ using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
-    public GameObject inventoryPanel; // Reference to the panel
-    public GameObject inventorySlotPrefab; // Prefab for each slot
-    public List<Sprite> itemSprites; // List of item icons
-    
-    private List<GameObject> inventorySlots = new List<GameObject>();
+    public GameObject greenhouseInventory;
+    public GameObject solarInventory;
+    public GameObject wasteInventory;
+    public GameObject nuclearInventory;
+    public GameObject miningInventory;
+    public GameObject housingInventory;
+    public GameObject researchInventory;
 
-    void Start()
-    {
-        PopulateInventory();
+    public static InventoryManager singleton;
+
+    void Start() {
+        singleton = this;
     }
 
-    void PopulateInventory()
-    {
-        foreach (Sprite itemSprite in itemSprites)
-        {
-            // Instantiate a new slot
-            GameObject newSlot = Instantiate(inventorySlotPrefab, inventoryPanel.transform);
 
-            // Set the item's image
-            newSlot.GetComponent<Image>().sprite = itemSprite;
+    public static void OpenPanel(string buildingType) {
+        CloseAllPanel();
 
-            // Add the slot to the list
-            inventorySlots.Add(newSlot);
+        switch(buildingType) {
+            case "GREENHOUSE":
+                InventoryManager.singleton.greenhouseInventory.SetActive(true);
+                break;
+            case "SOLAR":
+                InventoryManager.singleton.solarInventory.SetActive(true);
+                break;
+            case "WASTE":
+                InventoryManager.singleton.wasteInventory.SetActive(true);
+                break;
+            case "NUCLEAR":
+                InventoryManager.singleton.nuclearInventory.SetActive(true);
+                break;
+            case "MINING":
+                InventoryManager.singleton.miningInventory.SetActive(true);
+                break;
+            case "HOUSING":
+                InventoryManager.singleton.housingInventory.SetActive(true);
+                break;
+            case "RESEARCH":
+                InventoryManager.singleton.researchInventory.SetActive(true);
+                break;
         }
+    }
+
+    public static void CloseAllPanel() {
+        InventoryManager.singleton.greenhouseInventory.SetActive(false);
+        InventoryManager.singleton.solarInventory.SetActive(false);
+        InventoryManager.singleton.wasteInventory.SetActive(false);
+        InventoryManager.singleton.nuclearInventory.SetActive(false);
+        InventoryManager.singleton.miningInventory.SetActive(false);
+        InventoryManager.singleton.housingInventory.SetActive(false);
+        InventoryManager.singleton.researchInventory.SetActive(false);
     }
 }
