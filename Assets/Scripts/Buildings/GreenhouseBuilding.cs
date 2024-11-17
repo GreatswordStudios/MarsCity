04130acf66732d88d7a31b49ac5446f6008709ff
Jack Drolet <79327056+J-Drolet@users.noreply.github.com>
2024-11-17 09:12:17 -0500

    Greenhouse doesn't "need" waste

--- Assets/Scripts/Buildings/GreenhouseBuilding.cs
+++ Assets/Scripts/Buildings/GreenhouseBuilding.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GreenhouseBuilding: Building
@@ -10,14 +11,14 @@ public class GreenhouseBuilding: Building
         Dictionary<string, float> drain = SceneMgr.gameDesignValues[BuildingType.GREENHOUSE];
         SceneMgr sceneMgr = SceneMgr.singleton;
 
-        if(sceneMgr.water > drain["water"] && sceneMgr.elec >= drain["elec"]  && sceneMgr.waste >= drain["waste"] && sceneMgr.availableWorkforce >= drain["populationCost"]) {
+        if(sceneMgr.water > drain["water"] && sceneMgr.elec >= drain["elec"] && sceneMgr.availableWorkforce >= drain["populationCost"]) {
             
             sceneMgr.water -= drain["water"];
             sceneMgr.oxygen -= drain["oxygen"];
             sceneMgr.biomass -= drain["biomass"];
             sceneMgr.elec -= drain["elec"];
             sceneMgr.buildingMats -= drain["buildingMats"];
-            sceneMgr.waste -= drain["waste"];
+            sceneMgr.waste -= Math.Min(drain["waste"], sceneMgr.waste); // so we don't go under 0
             sceneMgr.availableWorkforce -= drain["populationCost"];
             return;
         }
